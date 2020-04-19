8f038ad6e160aeef6e0a2c9535204a47e54b10b5
erblina <jakupierblina1@gmail.com>
2020-04-19 18:11:56 +0200

    added a list in fragments and a drawer layout for the main activity

--- app/src/main/java/com/example/foodorder/Fragments/PastaFragment.java
+++ app/src/main/java/com/example/foodorder/Fragments/PastaFragment.java
@@ -5,19 +5,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.example.foodorder.R;
 
 
 
 
-public class PastaFragment extends Fragment {
+public class PastaFragment extends ListFragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pasta, container, false);
+        //return inflater.inflate(R.layout.fragment_pasta, container, false);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.pasta));
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
