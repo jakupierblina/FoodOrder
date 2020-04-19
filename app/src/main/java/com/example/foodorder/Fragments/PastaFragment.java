06cf8af91cca86061954c75faced60bda084b877
Zafar Yab Subhani <61932990+yaab5825@users.noreply.github.com>
2020-04-19 02:52:19 +0200

    Pizza and Pasta Fragments
    plz let me know if I have to change something in my code.

--- app/src/main/java/com/example/foodorder/Fragments/PastaFragment.java
+++ app/src/main/java/com/example/foodorder/Fragments/PastaFragment.java
@@ -5,19 +5,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.foodorder.R;
 
 
 
-
-public class PastaFragment extends Fragment {
-
+public class PastaFragment extends Fragment implements View.OnClickListener
+{
+    private Button button1,button2,button3,button4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pasta, container, false);
+                             Bundle savedInstanceState)
+    {
+
+        View v = inflater.inflate(R.layout.fragment_pasta, container, false);
+        Button button1 = (Button) v.findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        Button button2 = (Button) v.findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        Button button3 = (Button) v.findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+        Button button4 = (Button) v.findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+        return v;
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.button1:
+                Toast.makeText(getActivity().getApplicationContext(), "Italian Pasta is selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(getActivity().getApplicationContext(), "White Sauce Pasta is selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(getActivity().getApplicationContext(), "Basil Pasta is selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(getActivity().getApplicationContext(), "Primavera Pasta is selected", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
     }
 }
--- app/src/main/java/com/example/foodorder/Fragments/PizzaFragment.java
+++ app/src/main/java/com/example/foodorder/Fragments/PizzaFragment.java
@@ -5,17 +5,54 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.foodorder.R;
 
 
-public class PizzaFragment extends Fragment {
 
+public class PizzaFragment extends Fragment implements View.OnClickListener {
+
+    private Button button1,button2,button3,button4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pizza, container, false);
+                             Bundle savedInstanceState)
+    {
+
+        View v = inflater.inflate(R.layout.fragment_pizza, container, false);
+        Button button1 = (Button) v.findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        Button button2 = (Button) v.findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        Button button3 = (Button) v.findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+        Button button4 = (Button) v.findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.button1:
+                Toast.makeText(getActivity().getApplicationContext(), "Chicago Pizza is selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(getActivity().getApplicationContext(), "Chicken Pizza is selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(getActivity().getApplicationContext(), "Margherita Pizza is selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(getActivity().getApplicationContext(), "Italian Pizza is selected", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
     }
 }
