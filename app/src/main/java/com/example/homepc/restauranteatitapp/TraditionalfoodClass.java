3cdad37cf1119fc17db3df5522463c8f75e59f2b
jakupierblina <33768451+jakupierblina@users.noreply.github.com>
2020-05-03 18:30:50 +0200

    Update TraditionalfoodClass.java

--- app/src/main/java/com/example/homepc/restauranteatitapp/TraditionalfoodClass.java
+++ app/src/main/java/com/example/homepc/restauranteatitapp/TraditionalfoodClass.java
@@ -1,8 +1,5 @@
 package com.example.homepc.restauranteatitapp;
 
-/**
- * Created by HomePC on 1/9/2018.
- */
 
 public class TraditionalfoodClass {
 
@@ -12,16 +9,12 @@ public class TraditionalfoodClass {
     public String Itemquantity;
 
 
-
     public TraditionalfoodClass(String iName, String iPrice, int iImage,String iquantity)
     {
         Itemname = iName;
         Itemprice = iPrice;
         Itemimage = iImage;
         Itemquantity = iquantity;
-
-
-
     }
     public String getItemName() {
         return Itemname;
@@ -32,10 +25,4 @@ public class TraditionalfoodClass {
     public int getImageResourceId() {return Itemimage;}
 
     public String getItemquantity() {return Itemquantity;}
-
-
-
-
-
-
 }
